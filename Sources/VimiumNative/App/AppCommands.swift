506bc52dbab0d7c02bec21c831bf452874cb91d0
Amanali Abilkhan <a.abilkhan024@gmail.com>
2025-02-21 18:56:42 +0500

    feat: app commands with native swift

--- Sources/VimiumNative/App/AppCommands.swift
+++ Sources/VimiumNative/App/AppCommands.swift
@@ -4,34 +4,59 @@ import Foundation
 @MainActor
 final class AppCommands {
   static let shared = AppCommands()
+
+  let appPath = CommandLine.arguments[0]
+  let daemonPath = "/tmp/vimium-native-daemon"
+  let daemonLogPath = "/tmp/vimium-native-daemon.log"
+  let fs = FileManager.default
+
+  enum Action: String {
+    case daemon = "daemon"
+    case kill = "kill"
+  }
+
   private init() {}
 
   func daemonize() {
-    var pid: pid_t = 0
-    let path = "/usr/bin/nohup"
-    let args: [String] = ["nohup", CommandLine.arguments[0]]
-    var cArgs = args.map { e in strdup(e) } + [nil]
-
-    let status = posix_spawn(&pid, path, nil, nil, &cArgs, nil)
-    cArgs.forEach { e in free(e) }
-
-    if status == 0 {
-      print("Daemon started with PID: \(pid)")
-      exit(0)
-    } else {
-      print("Failed to spawn daemon")
-      exit(1)
+    let p = Process()
+    if !fs.fileExists(atPath: daemonLogPath) {
+      fs.createFile(atPath: daemonLogPath, contents: nil, attributes: nil)
+    }
+    p.standardOutput = FileHandle(forWritingAtPath: daemonLogPath)
+    p.standardError = FileHandle(forWritingAtPath: daemonLogPath)
+    p.executableURL = URL(fileURLWithPath: appPath)
+    try? p.run()
+    do {
+      try "\(p.processIdentifier)".write(toFile: daemonPath, atomically: true, encoding: .utf8)
+      print("Started in daemon mode \(p.processIdentifier)")
+    } catch {
+      print("Failed to write daemon pid, terminating")
+      p.terminate()
     }
   }
 
-  func kill() {
-    let app = CommandLine.arguments[0]
-    let result = Process()
-    result.launchPath = "/usr/bin/killall"
-    result.arguments = [URL(fileURLWithPath: app).lastPathComponent]
-    try? result.run()
-    result.waitUntilExit()
-    exit(0)
+  func killRunning() -> Bool {
+    do {
+      let content = try String(contentsOfFile: daemonPath, encoding: .utf8)
+      guard let pid = Int32(content) else {
+        print("Impossible case, daemon file doesn't contain valid pid")
+        exit(1)
+      }
+      try fs.removeItem(atPath: daemonLogPath)
+      kill(pid, SIGKILL)
+      return true
+    } catch {
+      return false
+    }
+
+    // let p = Process()
+    // p.launchPath = Deps.killall.rawValue
+    // p.processIdentifier
+    // let name = URL(fileURLWithPath: appPath).lastPathComponent
+    // // p.arguments = ["\"\(name) \(Action.daemon.rawValue)\""]
+    // p.arguments = [name]
+    // try? p.run()
+    // p.waitUntilExit()
   }
 
   func runMenu() {
@@ -48,10 +73,15 @@ final class AppCommands {
   }
 
   func run() {
-    if CommandLine.arguments.contains("kill") {
-      kill()
-    } else if CommandLine.arguments.contains("daemon") {
+    if CommandLine.arguments.contains(Action.kill.rawValue) {
+      if !killRunning() {
+        print("Didn't find any daemons")
+      }
+      exit(0)
+    } else if CommandLine.arguments.contains(Action.daemon.rawValue) {
+      let _ = killRunning()
       daemonize()
+      exit(0)
     } else {
       runMenu()
     }
